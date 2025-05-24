83a5b9972aba5629ff663385413e6e2d5a1740f1
RinkiShara <rinki2001sharma@gmail.com>
2025-05-24 16:21:37 +0530

    create UserRoutes

---
+++ backend/src/controllers/userController.js
@@ -0,0 +1,12 @@
+const User = require('../models/userModel');
+
+// Controller to insert user data
+exports.createUser = async (req, res) => {
+    try {
+        const user = new User(req.body);
+        const savedUser = await user.save();
+        res.status(201).json(savedUser);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+};
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,15 +1,19 @@
 
 const express = require('express')
 const connectDB = require("./config/db");
+const userRoutes = require('./routes/userRoutes'); 
 
 const app = express()
 const port = 3000
+app.use(express.json());
  connectDB()
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+app.use("/users", userRoutes);
 
+app
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
