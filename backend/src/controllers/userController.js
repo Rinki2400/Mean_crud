517c2eef9dc2a6ab3a8d387fb4e13966b1556fa7
RinkiShara <rinki2001sharma@gmail.com>
2025-05-25 00:14:34 +0530

    getAllUser Api created

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -10,3 +10,26 @@ exports.createUser = async (req, res) => {
         res.status(400).json({ error: err.message });
     }
 };
+
+exports.getAllUsers = async (req, res) => {  
+    try {  
+        const users = await User.find();  
+        res.status(200).json(users);  
+    } catch (err) {  
+        console.error("Error fetching users:", err.message); // Debugging log
+        res.status(500).json({ error: "Internal Server Error" });  
+    }  
+};
+//  exports.deleteUser = async (req,res) =>{
+//     try {
+//         const userId = req.params.id
+//         const deleteUser = await User.findByIdAndDelete(userId)
+//         if(!deleteUser) {
+//             return res.status(404).json({ message: "User not found" });
+//         }
+//         res.status(200).json({ message: "User deleted successfully" });
+//     } catch (error) {
+//         console.error("Error deleting user:", error.message); 
+//         res.status(500).json({ error: "Internal Server Error" });
+//     }
+//  }
--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { createUser } = require('../controllers/userController'); // Import controller
+const { createUser,getAllUsers} = require('../controllers/userController'); // Import controller
 
 const router = express.Router();
 
 // Correctly use createUser function
 router.post('/', createUser);
+router.get('/', getAllUsers);
 
 module.exports = router;
