3d76f1ddae5b3a96312f71983944302656c2de07
RinkiShara <rinki2001sharma@gmail.com>
2025-05-25 15:02:46 +0530

    creata user compont/services

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { UsersComponent } from './users/users.component';
+import { UsersFormComponent } from './users/users-form/users-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component:UsersComponent
+    },
+    {
+        path: 'users',
+        component: UsersComponent
+    },
+     {
+       path: 'users/add',
+        component: UsersFormComponent
+    }
+];
