0f37236aef815c02fe2075b9a4b4f2194fe94020
RinkiShara <rinki2001sharma@gmail.com>
2025-05-25 16:27:28 +0530

    edit form

--- frontend/src/app/users/users-form/users-form.component.ts
+++ frontend/src/app/users/users-form/users-form.component.ts
@@ -1,11 +1,33 @@
 import { Component } from '@angular/core';
+import { FormGroup, Validators, FormBuilder, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-users-form',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './users-form.component.html',
-  styleUrl: './users-form.component.css'
+  styleUrl: './users-form.component.css',
 })
 export class UsersFormComponent {
+  userForm: FormGroup;
+  constructor(private fb: FormBuilder) {
+    this.userForm = this.fb.group({
+      username: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+      age: ['', Validators.min(0)],
+      address: ['', Validators.required],
+    });
+  }
+  submitForm() {
+    if (this.userForm.valid) {
+      console.log('Form Data:', this.userForm.value);
+    } else {
+      console.log('Form is invalid');
+    }
+  }
+  
+  resetForm() {
+    this.userForm.reset();
+  }
 
-}
+};
