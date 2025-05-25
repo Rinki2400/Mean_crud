4dc410dd21f397f5f1142a943ce0758efde85903
RinkiShara <rinki2001sharma@gmail.com>
2025-05-26 00:38:52 +0530

    api updated

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -3,7 +3,7 @@ const { createUser, getAllUsers, deleteUser ,updateUser } = require('../controll
 
 const router = express.Router();
 
-router.post('/', createUser);
+router.post('/user', createUser);
 router.get('/user', getAllUsers);
 router.delete('/:id', deleteUser);
 router.put('/:id', updateUser); 
